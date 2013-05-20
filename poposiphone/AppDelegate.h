d7c86274f076e3f89d9db5174294c06795c376c1
Brandon Liu <bdon@bdon.org>
2013-05-20 14:59:39 -0700

    Initial Commit

---
+++ poposiphone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  poposiphone
+//
+//  Created by Brandon Liu on 5/20/13.
+//  Copyright (c) 2013 bdon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ poposiphone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  poposiphone
+//
+//  Created by Brandon Liu on 5/20/13.
+//  Copyright (c) 2013 bdon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
