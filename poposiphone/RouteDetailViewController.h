3eebca43e3ee3014d72ad6047085b8b0994a397e
Brandon Liu <bdon@bdon.org>
2013-05-31 00:42:59 -0700

    add route dashed lines to route detail view.

--- poposiphone/RouteDetailViewController.h
+++ poposiphone/RouteDetailViewController.h
@@ -1,6 +1,7 @@
 #import <UIKit/UIKit.h>
+#import <MapBox/MapBox.h>
 #import "Route.h"
 
-@interface RouteDetailViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface RouteDetailViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, RMMapViewDelegate>
 - (id)initWithRoute:(Route *)route;
 @end
--- poposiphone/RouteDetailViewController.m
+++ poposiphone/RouteDetailViewController.m
@@ -1,7 +1,6 @@
 #import "RouteDetailViewController.h"
 #import "Space.h"
 #import "SpaceViewController.h"
-#import <MapBox/MapBox.h>
 
 @interface RouteDetailViewController ()
 @property (strong, nonatomic) Route *route;
@@ -25,6 +24,25 @@
     
     RMMapBoxSource *onlineSource = [[RMMapBoxSource alloc] initWithMapID:@"cdawson.map-0ymh0yul"];
     self.mapView.tileSource = onlineSource;
+    self.mapView.delegate = self;
+    self.mapView.showLogoBug = NO;
+    self.mapView.draggingEnabled = NO;
+    
+    
+//    for (Space *space in self.spaceRepository.spaces) {
+//        RMAnnotation *annotation = [RMAnnotation annotationWithMapView:mapView coordinate:space.coordinate andTitle:space.identifier];
+//        annotation.userInfo = @{@"type":@"space",@"obj":space};
+//        [mapView addAnnotation:annotation];
+//    }
+//    
+
+    CLLocationCoordinate2D firstCoord;
+    [self.route.coordinates[0] getValue:&firstCoord];
+    RMAnnotation *annotation = [RMAnnotation annotationWithMapView:self.mapView coordinate:firstCoord andTitle:@"Route"];
+    annotation.userInfo = @{@"type":@"route",@"obj":self.route};
+    [self.mapView addAnnotation:annotation];
+    
+    self.mapView.centerCoordinate = firstCoord;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -48,6 +66,32 @@
     Space *space = self.route.spaces[indexPath.row];
     SpaceViewController *spaceView = [[SpaceViewController alloc] initWithSpace:space];
     [self.navigationController pushViewController:spaceView animated:YES];
+}
+
+- (RMMapLayer *)mapView:(RMMapView *)mapView layerForAnnotation:(RMAnnotation *)annotation
+{
+    if ([annotation.userInfo[@"type"] isEqualToString:@"route"]) {
+        Route *route = (Route *)annotation.userInfo[@"obj"];
+        RMShape *routeShape = [[RMShape alloc] initWithView:mapView];
+        routeShape.scaleLineDash = NO;
+        routeShape.lineColor = [UIColor redColor];
+        routeShape.lineDashLengths = @[@(5.0)];
+        routeShape.lineWidth = 2.0;
+
+        BOOL moveTo = YES;
+        for (NSValue *coordValue in route.coordinates) {
+            CLLocationCoordinate2D coord;
+            [coordValue getValue:&coord];
+            if (moveTo) {
+                [routeShape moveToCoordinate:coord];
+            } else {
+                [routeShape addLineToCoordinate:coord];
+            }
+            
+            moveTo = NO;
+        }
+        return routeShape;
+    }
 }
 
 @end
